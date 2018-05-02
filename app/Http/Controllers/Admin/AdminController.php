7caa21a4ea0659ab033de194fbe58b699bcc167c
zzj89082 <1529333231@qq.com>
2018-05-02 16:03:16 +0800

    make改

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,7 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Config;
 use App\Models\Rollimg;
-
+use App\Models\Admin\User;
 class AdminController extends Controller
 {
     /**
@@ -22,7 +22,19 @@ class AdminController extends Controller
         $data = Config::find(1);
         $rollimg = Rollimg::get();
         session(['data'=>$data]);
-        return view('admin/index',['data'=>$data,'rollimg'=>$rollimg]); 
+        //统计用户
+        $pu_data = User::where('Upower',0)->count();  //普通用户
+        $V_data = User::where('Upower',2)->count();  //大V用户
+        $qiye_data = User::where('Upower',4)->count();  //企业用户
+        $Vip_data = User::where('Upower',1)->count(); //VIP用户
+
+        return view('admin/index',['data'=>$data,
+            'rollimg'=>$rollimg,
+            'pu_data'=>$pu_data,
+            'V_data'=>$V_data,
+            'qiye_data'=>$qiye_data,
+            'Vip_data'=>$Vip_data
+        ]); 
     }
 
 }
